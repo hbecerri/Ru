c028be2e9e8f621ae9da87d5530f0b4619a5b116
hbecerri <35848309+hbecerri@users.noreply.github.com>
2020-09-14 11:49:58 -0500

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -7,11 +7,10 @@ rm -f ZprimeWide_limits.txt
 rm -f ZprimeUltraWide_limits.txt
 
 declare -a arr=("ZprimeNarrow0500" "ZprimeNarrow0750" "ZprimeNarrow1000" "ZprimeNarrow1250" "ZprimeNarrow1500" "ZprimeNarrow2000" "ZprimeNarrow2500" "ZprimeNarrow3000" "ZprimeNarrow4000" "ZprimeNarrow4500" "ZprimeNarrow5000" "ZprimeNarrow6000" "ZprimeNarrow6500" "ZprimeNarrow7000")
-#declare -a arr=("ZprimeNarrow2000")
+
 for i in "${arr[@]}"
 do
 	python TxtDC_limits.py ${i}
-	combineCards.py SR1T_muon=SR1T_muon.txt SR0T_muon=SR0T_muon.txt > Signal_Region.txt
         combineCards.py SR1T_muon=SR1T_muon.txt SR0T_muon=SR0T_muon.txt CR1_muon=CR1_muon.txt CR2_muon=CR2_muon.txt > Signal_Region.txt
         combine -M AsymptoticLimits Signal_Region.txt --noFitAsimov >> ZprimeNarrow_limits.txt
         sed -i 's/Done.*//g' ZprimeNarrow_limits.txt
@@ -28,4 +27,4 @@ do
         sed -i '/^$/d' ZprimeNarrow_limits.txt
 done
 
-root -l -b -q Brazil.C
+
